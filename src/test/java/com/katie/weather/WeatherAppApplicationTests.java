dc4d9e6812c57ade28ec16a2ca82798f4522fa72
Katie Harte <katherine.harte@grainger.com>
2019-06-07 16:01:16 -0500

    Added test for checking if the temperature is formatted correctly. Cleaned up some stray comments. Also, I realized I'm not writing unit tests correctly--I need to create mock objects to fix this.

--- src/test/java/com/katie/weather/WeatherAppApplicationTests.java
+++ src/test/java/com/katie/weather/WeatherAppApplicationTests.java
@@ -7,7 +7,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.validation.constraints.AssertTrue;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+These are all wrong! I've been writing these unit tests that depend on the weather API--
+I need to re-write the tests using mock objects that simulate the API calls. These are
+not real unit tests.
+*/
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,20 +37,16 @@ public class WeatherAppApplicationTests {
 
 	@Test
 	public void currentChicagoTempIsAReasonableNumber() {
-//		Double temperatureDouble = Double.parseDouble(weatherModel.currentChicagoTemp());
 		Double temperatureDouble = weatherModel.currentChicagoTemp();
+
 		Assert.assertTrue(temperatureDouble > -20);
 		Assert.assertTrue(temperatureDouble < 120);
 	}
 
 	@Test
 	public void tempIsFormattedCorrectly() {
-
+		Assert.assertTrue(Pattern.matches("[0-9].*°F$", weatherModel.formattedTemp()));
 	}
 
-	@Test
-	public void fetchesSomethingForChicagoRainData() {
-		Assert.assertTrue(false);
-	}
 
-	}
+}
