86ac4b7b45b6ee64899c4c246accd125f422da1e
Katie Harte <katherine.harte@grainger.com>
2019-06-14 15:06:21 -0500

    Specified 'get' method for @RequestMapping methods.

--- src/main/java/com/katie/weather/WeatherController.java
+++ src/main/java/com/katie/weather/WeatherController.java
@@ -3,18 +3,19 @@ package com.katie.weather;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping(path = "/")
 public class WeatherController {
     private WeatherModel weatherModel = new WeatherModel();
 
-    @RequestMapping(path = "/katherine.harte@grainger.com")
+    @RequestMapping(path = "/katherine.harte@grainger.com", method = RequestMethod.GET)
     public String getKatiesInfo() {
         return "katie";
     }
 
-    @RequestMapping(path = "/weather/Chicago")
+    @RequestMapping(path = "/weather/Chicago", method = RequestMethod.GET)
     public String showChicagoWeather(Model model) {
         model.addAttribute("cityID", ApiStaticInfo.cityID);
         model.addAttribute("temperature", weatherModel.formattedTemp());
